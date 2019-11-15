40f254577805cfebdd46ff067dd449677c593036
lethalSid <senien.hicks@gmail.com>
2019-11-15 03:49:19 -0500

    used fetch api to load news to page

---
+++ src/components/News/News.jsx
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import NewSingle from './NewSingle.jsx';
+
+class News extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            news: [],
+        };
+    }
+
+    componentDidMount() {
+        const url = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=3b360f20444d4d618d6d62a90874da87';
+
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({
+                    news: data.articles
+                })
+            })
+            .catch((error) => console.log(error));
+    }
+    renderItems() {
+        return this.state.news.map((item) => (
+            <NewSingle key={item.id} item={item} />
+        ));
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.renderItems()}
+            </ul>
+        );
+    }
+}
+
+export default News;
